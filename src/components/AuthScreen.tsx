f6ce20857adab691e9a04ff8de085eb798975f75
felipesergios <felipe.sergio2011@gmail.com>
2022-05-05 16:10:54 -0300

    first commit

---
+++ src/components/AuthScreen.tsx
@@ -0,0 +1,78 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
+  import {ColorModeSwitcher} from '../ColorModeSwitcher'
+  
+  export default function AuthScreen() {
+    let history = useHistory()
+    function handleClick() {
+      history.push("/admin");
+    }
+
+    return (
+
+      
+
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'}>Sistema de prazos contratuais</Heading>
+            <Text fontSize={'lg'} color={'gray.600'}>
+              Faça o acompanhamento e gestão de tempo  ✌️
+            </Text>
+            <ColorModeSwitcher/>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input type="password" />
+              </FormControl>
+              <Stack spacing={10}>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}>
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={'blue.400'}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  onClick={handleClick}
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                  Sign in
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    );
+  }
